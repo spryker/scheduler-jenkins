6b80a6639c7073b3854093f91a10227d80b4e3dc
Mike Kalinin <mike.kalinin@spryker.com>
2019-05-30 20:03:31 +0300

    SC-1032: Refactoring: decomposition, renaming. Adding additional filters. Removing dependency on store.

--- src/Spryker/Zed/SchedulerJenkins/Business/Api/Exception/WrongJenkinsConfiguration.php
+++ src/Spryker/Zed/SchedulerJenkins/Business/Api/Exception/WrongJenkinsConfiguration.php
@@ -9,6 +9,6 @@ namespace Spryker\Zed\SchedulerJenkins\Business\Api\Exception;
 
 use Exception;
 
-class JenkinsBaseUrlNotFound extends Exception
+class WrongJenkinsConfiguration extends Exception
 {
 }
